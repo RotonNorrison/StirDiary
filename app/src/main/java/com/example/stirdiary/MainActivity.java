f8cb44f47322fe569b2a7e6b3d4f77e40ec89e23
roton <544703430@qq.com>
2020-05-11 13:08:06 +0800

    Test_Version

---
+++ app/src/main/java/com/example/stirdiary/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.stirdiary;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+    }
+}
