a86ca637b2dc501576f1d10057cd78eae8b8d64f
RotonNorrison <544703430@qq.com>
2020-05-12 10:40:38 +0800

    CSR_Mod1

--- app/src/main/java/com/example/stirdiary/MainActivity.java
+++ app/src/main/java/com/example/stirdiary/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.stirdiary;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
+        //阅读自己日记——单机酒柜事件
+        View btn_for_reading = this.getLayoutInflater().inflate((R.layout.activity_main), null);
+        btn_for_reading.findViewById(R.id.homepage_ownDiaryReading);
+        btn_for_reading.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it_for_reading = new Intent(MainActivity.this, OwnDiaryReading.class);
+                startActivity(it_for_reading);
+            }
+        });
+
     }
 }
