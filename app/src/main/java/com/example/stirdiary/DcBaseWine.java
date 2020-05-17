bd932a71ac5007d15ce18cac9241e63665441456
xuehzjing <xuehzjing@outlook.com>
2020-05-17 13:06:09 +0800

    diary creation pages added

--- app/src/main/java/com/example/stirdiary/DcBaseWine.java
+++ app/src/main/java/com/example/stirdiary/DcBaseWine.java
@@ -1,6 +1,10 @@
 package com.example.stirdiary;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DcBaseWine extends AppCompatActivity {
@@ -8,5 +12,16 @@ public class DcBaseWine extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.diary_creation_basewine);
+        //下一步按钮事件绑定
+        Button btn_for_continue_to_add_text;
+        btn_for_continue_to_add_text = findViewById(R.id.chooseBase_nextbtn);
+        btn_for_continue_to_add_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it_for_add_text = new Intent(DcBaseWine.this, DcText.class);
+                startActivity(it_for_add_text);
+                finish();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/stirdiary/DiaryCreate.java
@@ -0,0 +1,27 @@
+package com.example.stirdiary;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class DiaryCreate extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.diary_creation_page);
+        //开始调酒按钮事件绑定
+        Button btn_for_start;
+        btn_for_start = findViewById(R.id.diaryCreation_startbtn);
+        btn_for_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it_for_create1 = new Intent(DiaryCreate.this, DcBottle.class);
+                startActivity(it_for_create1);
+                finish();
+            }
+        });
+    }
+}
