9443f7d05e10cf46b734a88b048c2bf78ce5746a
RotonNorrison <544703430@qq.com>
2020-05-18 11:20:47 +0800

    CSR_Mod7(完善了Diary类，完善了页面跳转）

--- app/src/main/java/com/example/stirdiary/Diary.java
+++ app/src/main/java/com/example/stirdiary/Diary.java
@@ -1,13 +1,63 @@
 package com.example.stirdiary;
 
 import java.util.Dictionary;
+import java.util.HashMap;
 
 public class Diary {
     private String diary_title;
     private int bottle_kind;
     //基酒种类及对应的量
-    private Dictionary wine_kind;
+    private HashMap<Integer, Integer> wine_kind;
     private String text;
     private int decoration;
     private boolean share_state;
+
+
+    public String getDiary_title() {
+        return diary_title;
+    }
+
+    public void setDiary_title(String diary_title) {
+        this.diary_title = diary_title;
+    }
+
+    public int getBottle_kind() {
+        return bottle_kind;
+    }
+
+    public void setBottle_kind(int bottle_kind) {
+        this.bottle_kind = bottle_kind;
+    }
+
+    public HashMap<Integer, Integer> getWine_kind() {
+        return wine_kind;
+    }
+
+    public void setWine_kind(int wine_kind, int volume) {
+        this.wine_kind.put(wine_kind, volume);
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getDecoration() {
+        return decoration;
+    }
+
+    public void setDecoration(int decoration) {
+        this.decoration = decoration;
+    }
+
+    public boolean isShare_state() {
+        return share_state;
+    }
+
+    public void setShare_state(boolean share_state) {
+        this.share_state = share_state;
+    }
 }
