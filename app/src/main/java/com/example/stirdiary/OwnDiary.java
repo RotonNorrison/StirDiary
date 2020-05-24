f24d2c7b8974730d69e865f3727e271f0a9590d8
RotonNorrison <544703430@qq.com>
2020-05-24 18:20:07 +0800

    CSR_Mod10（修正了后端部分）

--- app/src/main/java/com/example/stirdiary/OwnDiary.java
+++ app/src/main/java/com/example/stirdiary/OwnDiary.java
@@ -8,14 +8,29 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
+import java.util.List;
+
 public class OwnDiary extends AppCompatActivity {
     private LinearLayout container;
 
-    public void addWine(int num) {
+    public void addWine() {
         String imgsrc;
         final String contentsrc;
+        DiaryFileHelper mDFH = new DiaryFileHelper(getApplicationContext());
+        List<Diary> Dlist = null;
+        int temp;
+        try {
+            Dlist = mDFH.readDiaryListFromFile("diary_list_storage");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         contentsrc = "test";
-        for (int i = 0; i < num; i++) {
+        int num = Dlist.size();
+        temp = Math.min(num, 3);
+        num -= 3;
+        for (int i = 0; i < temp; i++) {
+            final Diary Diarytemp = Dlist.get(i);
             container = findViewById(R.id.container1);
             ImageView child = new ImageView(this);
             child.setImageResource(R.drawable.diaryicon);
@@ -26,7 +41,47 @@ public class OwnDiary extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     Intent it = new Intent(OwnDiary.this, DiaryReading.class);
-                    it.putExtra("filename", contentsrc);
+                    it.putExtra("file", Diarytemp);
+                    startActivity(it);
+                }
+            });
+            container.addView(child);
+        }
+        temp = Math.min(num, 3);
+        num -= 3;
+        for (int i = 0; i < temp; i++) {
+            final Diary Diarytemp = Dlist.get(i + 3);
+            container = findViewById(R.id.container2);
+            ImageView child = new ImageView(this);
+            child.setImageResource(R.drawable.diaryicon);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(400, 400);
+            params.setMargins(30, 30, 30, 30);
+            child.setLayoutParams(params);
+            child.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent it = new Intent(OwnDiary.this, DiaryReading.class);
+                    it.putExtra("file", Diarytemp);
+                    startActivity(it);
+                }
+            });
+            container.addView(child);
+        }
+        temp = Math.min(num, 3);
+        num -= 3;
+        for (int i = 0; i < temp; i++) {
+            final Diary Diarytemp = Dlist.get(i + 6);
+            container = findViewById(R.id.container3);
+            ImageView child = new ImageView(this);
+            child.setImageResource(R.drawable.diaryicon);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(400, 400);
+            params.setMargins(30, 30, 30, 30);
+            child.setLayoutParams(params);
+            child.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent it = new Intent(OwnDiary.this, DiaryReading.class);
+                    it.putExtra("file", Diarytemp);
                     startActivity(it);
                 }
             });
@@ -38,6 +93,6 @@ public class OwnDiary extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.own_diary_page);
-        addWine(3);
+        addWine();
     }
 }
